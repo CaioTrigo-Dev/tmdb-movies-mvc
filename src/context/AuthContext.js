77ce5c14b9288a18ef157164af89662e8572b373
Caio Cesar <caiorj95@gmail.com>
2026-02-05 21:36:40 -0300

    feature - add AuthContextLogin

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,10 +2,20 @@ import { createContext, useState } from "react";
 
 
 const AuthContext = createContext();
+
 export default function AuthProvider({children}){
     const [user, setUser] = useState(null);
+
+    function login(username, password){
+        if(username === 'admin' && password === 'admin'){
+            setUser('Administrador');
+        }
+    }
+    function logout(){
+        setUser(null);
+    }
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{user, login, logout}}>
             {children}
         </AuthContext.Provider>
     )
